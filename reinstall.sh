a1d613547111c0af766350758b4de6ba4920591e
Francis Araujo <francis.araujo@gmail.com>
2023-06-25 07:46:33 -0300

    set worker label

--- reinstall.sh
+++ reinstall.sh
@@ -12,3 +12,11 @@ ansible-playbook -i ./inventory/k3s-cluster/hosts.ini site.yml
 printf "Refreshing local kucectl config\n"
 printf "#######################################################\n\n"
 scp khadas@192.168.15.73:~/.kube/config ~/.kube/config
+
+printf "Processing all nodes...\n"
+printf "#######################################################\n\n"
+
+#iterate over all nodes listed by kubectl
+for ns in $(kubectl get nodes -l '!node-role.kubernetes.io/master' --no-headers | awk '{print $1}'); do
+    kubectl label node ${ns} node-role.kubernetes.io/worker 
+done
